9d7813a766d5e364c00cb40f386f143b8f6f4324
Bo Xin <bxin@gmto.org>
2023-10-25 01:30:03 +0000

    surf2IQ/surf2IQ_CFD.ipynb

--- M1S_tools.py
+++ M1S_tools.py
@@ -39,111 +39,118 @@ saID_sw = np.loadtxt('saID_sw.txt')
 print('## bending modes & influence matrices etc from Buddy #####################')
 dataFolder = '/Users/bxin/Library/CloudStorage/OneDrive-SharedLibraries-GMTOCorp/M1S Portal - Documents'
 
-#read SA data
-dfSA = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/actCoords.mat')
-sax_ml = dfSA['yAct']/1e3 #turn into meter #swap x/y to get to M1B (M1DCS uses M1B!!!)
-say_ml = dfSA['xAct']/1e3 #turn into meter
-print('ML actuators = ', len(sax_ml), len(say_ml))
+try:
+    #read SA data
+    dfSA = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/actCoords.mat')
+    sax_ml = dfSA['yAct']/1e3 #turn into meter #swap x/y to get to M1B (M1DCS uses M1B!!!)
+    say_ml = dfSA['xAct']/1e3 #turn into meter
+    print('ML actuators = ', len(sax_ml), len(say_ml))
 
-#read Afz (Fz influence matrix)
-df = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/influenceFunctions.mat')
-Afn_ml = df['interactionMat']
-fv_ml = df['forceMat'] #fv = fv^T
-print('Afn = ',Afn_ml.shape)
-print('fv = ', fv_ml.shape)
-# this is Afz only; it is 6991 x 165.
+    #read Afz (Fz influence matrix)
+    df = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/influenceFunctions.mat')
+    Afn_ml = df['interactionMat']
+    fv_ml = df['forceMat'] #fv = fv^T
+    print('Afn = ',Afn_ml.shape)
+    print('fv = ', fv_ml.shape)
+    # this is Afz only; it is 6991 x 165.
 
-#read Fz Bending Mode
-mat = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/SVD.mat')
-UMat_ml = mat['U']
-SMat_ml = mat['S']
-VMat_ml = mat['V']
-print('U matrix', UMat_ml.shape)
+    #read Fz Bending Mode
+    mat = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/SVD.mat')
+    UMat_ml = mat['U']
+    SMat_ml = mat['S']
+    VMat_ml = mat['V']
+    print('U matrix', UMat_ml.shape)
 
-#read FEA nodes data
-mat = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/nodeCoords.mat')
-nodex_ml = mat['y']/1e3 #turn into meter #swap x/y to get to M1B (M1DCS uses M1B!!!)
-nodey_ml = mat['x']/1e3 #turn into meter
-print('N node = ', len(nodex_ml))
+    #read FEA nodes data
+    mat = scipy.io.loadmat(dataFolder+'/M1 Testing/RFCML Optical Testing/bending modes/nodeCoords.mat')
+    nodex_ml = mat['y']/1e3 #turn into meter #swap x/y to get to M1B (M1DCS uses M1B!!!)
+    nodey_ml = mat['x']/1e3 #turn into meter
+    print('N node = ', len(nodex_ml))
 
-############normalize bending modes to RMS = 1um ###################
-UMat_ml *= np.sqrt(UMat_ml.shape[0])
-for modeID in range(1, UMat_ml.shape[1]+1):
-    VMat_ml[:, modeID-1] *= 1e3/SMat_ml[modeID-1, modeID-1]*np.sqrt(UMat_ml.shape[0])
-    #1e3 due to nanometer to micron conversion; RFCML mode shapes are in nanometers
-
+    ############normalize bending modes to RMS = 1um ###################
+    UMat_ml *= np.sqrt(UMat_ml.shape[0])
+    for modeID in range(1, UMat_ml.shape[1]+1):
+        VMat_ml[:, modeID-1] *= 1e3/SMat_ml[modeID-1, modeID-1]*np.sqrt(UMat_ml.shape[0])
+        #1e3 due to nanometer to micron conversion; RFCML mode shapes are in nanometers
+except FileNotFoundError:
+    print('Data not exist. Are you sure they are there?')
+    
 print('## bending modes & influence matrices etc from Trupti #####################')
 dataFolder = '/Users/bxin/Library/CloudStorage/OneDrive-SharedLibraries-GMTOCorp/M1S Portal - Documents'
 
-#read SA data
-dfSA = pd.read_excel(dataFolder+'/2.4 Utilities/03. Utilities Distribution/utility_mapping_M1B_labels-16-Feb-2023.xlsx')
-sax = np.array(dfSA['x_m']) #in M1B
-say = np.array(dfSA['y_m'])
-saz = np.array(dfSA['z_m'])
-saID = np.array(dfSA['LSNo'])
-saReqMaxFx_N = np.array(dfSA['ReqMaxFx_N']) #max Fx the SA is allowed to produce??
-nact = len(dfSA)
-print('N actuators = ', nact)
+try:
+    #read SA data
+    dfSA = pd.read_excel(dataFolder+'/2.4 Utilities/03. Utilities Distribution/utility_mapping_M1B_labels-16-Feb-2023.xlsx')
+    sax = np.array(dfSA['x_m']) #in M1B
+    say = np.array(dfSA['y_m'])
+    saz = np.array(dfSA['z_m'])
+    saID = np.array(dfSA['LSNo'])
+    saReqMaxFx_N = np.array(dfSA['ReqMaxFx_N']) #max Fx the SA is allowed to produce??
+    nact = len(dfSA)
+    print('N actuators = ', nact)
 
-#read Afz (Fz influence matrix)
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afz-13-Apr-2023.csv', header=None)
-Afz = np.array(df)
-print('Afz = ',Afz.shape)
-# this is Afz only; it is 27685 x 170.
+    #read Afz (Fz influence matrix)
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afz-13-Apr-2023.csv', header=None)
+    Afz = np.array(df)
+    print('Afz = ',Afz.shape)
+    # this is Afz only; it is 27685 x 170.
 
-#read Afx (Fx influence matrix)
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afx-24-Jul-2023.csv', header=None)
-Afx = np.array(df)
-print('Afx = ', Afx.shape)
-#read Afy (Fy influence matrix)
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afy-24-Jul-2023.csv', header=None)
-Afy = np.array(df)
-print('Afy = ', Afy.shape)
+    #read Afx (Fx influence matrix)
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afx-24-Jul-2023.csv', header=None)
+    Afx = np.array(df)
+    print('Afx = ', Afx.shape)
+    #read Afy (Fy influence matrix)
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/Afy-24-Jul-2023.csv', header=None)
+    Afy = np.array(df)
+    print('Afy = ', Afy.shape)
 
-#read Fz Bending Modes & forces
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/U-13-Apr-2023.csv', header=None)
-UMat = np.array(df)
-print('U matrix', UMat.shape)
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/V-13-Apr-2023.csv', header=None)
-VMat = np.array(df)
-print('V matrix', VMat.shape)
-df = pd.read_csv(dataFolder+'/influnce_matrix_files/S-13-Apr-2023.csv', header=None)
-SMat = np.array(df)
-print('S matrix', SMat.shape)
+    #read Fz Bending Modes & forces
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/U-13-Apr-2023.csv', header=None)
+    UMat = np.array(df)
+    print('U matrix', UMat.shape)
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/V-13-Apr-2023.csv', header=None)
+    VMat = np.array(df)
+    print('V matrix', VMat.shape)
+    df = pd.read_csv(dataFolder+'/influnce_matrix_files/S-13-Apr-2023.csv', header=None)
+    SMat = np.array(df)
+    print('S matrix', SMat.shape)
 
-#read FEA nodes data
-mat = scipy.io.loadmat(dataFolder+'/influnce_matrix_files/NodeXYZsurface_meters.mat')
-nodeID = mat['NodeXYZsurface_meters'][:,0]
-nodex = mat['NodeXYZsurface_meters'][:,2] #swap x/y to get to M1B
-nodey = mat['NodeXYZsurface_meters'][:,1]
-nodez = mat['NodeXYZsurface_meters'][:,3]
-print('N node = ', len(nodeID))
+    #read FEA nodes data
+    mat = scipy.io.loadmat(dataFolder+'/influnce_matrix_files/NodeXYZsurface_meters.mat')
+    nodeID = mat['NodeXYZsurface_meters'][:,0]
+    nodex = mat['NodeXYZsurface_meters'][:,2] #swap x/y to get to M1B
+    nodey = mat['NodeXYZsurface_meters'][:,1]
+    nodez = mat['NodeXYZsurface_meters'][:,3]
+    print('N node = ', len(nodeID))
 
-############normalize bending modes to RMS = 1um ###################
-UMat *= np.sqrt(UMat.shape[0])
-for modeID in range(1, UMat.shape[1]+1):
-    VMat[:, modeID-1] *= 1e-6/SMat[modeID-1, modeID-1]*np.sqrt(UMat.shape[0])
-    #1e-6 due to meter to micron conversion
+    ############normalize bending modes to RMS = 1um ###################
+    UMat *= np.sqrt(UMat.shape[0])
+    for modeID in range(1, UMat.shape[1]+1):
+        VMat[:, modeID-1] *= 1e-6/SMat[modeID-1, modeID-1]*np.sqrt(UMat.shape[0])
+        #1e-6 due to meter to micron conversion
 
-npuck = np.zeros(nact)
-for i in range(nact):
-    if dfSA['LSActType'][i] == 0: #Single Axis on Single Puck
-        npuck[i] = 1
-    elif dfSA['LSActType'][i] == 1: #Single Axis on Dual Puck LS
-        npuck[i] = 2
-    elif dfSA['LSActType'][i] == 2: #Single Axis on triple puck LS
-        npuck[i] = 3
-    elif dfSA['LSActType'][i] == 30: #Single Axis on Single double triple LS puck
-        npuck[i] = 1
-    elif dfSA['LSActType'][i] == 31: #Single Axis on Single double triple LS bar
-        npuck[i] = 2
-    elif dfSA['LSActType'][i] == 40: #Triple Axis RH on triple puck LS
-        npuck[i] = 3
-    elif dfSA['LSActType'][i] == 41: #Triple Axis LH on triple puck LS
-        npuck[i] = 3
-    elif dfSA['LSActType'][i] == 5: #Triple Axis on quad puck LS
-        npuck[i] = 2
+    npuck = np.zeros(nact)
+    for i in range(nact):
+        if dfSA['LSActType'][i] == 0: #Single Axis on Single Puck
+            npuck[i] = 1
+        elif dfSA['LSActType'][i] == 1: #Single Axis on Dual Puck LS
+            npuck[i] = 2
+        elif dfSA['LSActType'][i] == 2: #Single Axis on triple puck LS
+            npuck[i] = 3
+        elif dfSA['LSActType'][i] == 30: #Single Axis on Single double triple LS puck
+            npuck[i] = 1
+        elif dfSA['LSActType'][i] == 31: #Single Axis on Single double triple LS bar
+            npuck[i] = 2
+        elif dfSA['LSActType'][i] == 40: #Triple Axis RH on triple puck LS
+            npuck[i] = 3
+        elif dfSA['LSActType'][i] == 41: #Triple Axis LH on triple puck LS
+            npuck[i] = 3
+        elif dfSA['LSActType'][i] == 5: #Triple Axis on quad puck LS
+            npuck[i] = 2
 
+except FileNotFoundError:
+    print('Data not exist. Are you sure they are there?')
+                
 def mlFvec2gmtFvec(mlFvec):
     '''
     convert a ML force vector (165x1) into a GMT force vector (170x1)
